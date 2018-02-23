8a37d3e30cfe828a8141aa7bef288a6c02b60604
wjdgjs0226 <36759231+wjdgjs0226@users.noreply.github.com>
2018-02-23 19:02:47 +0900

    Update RandomLetterChooser.java

--- AP CompSci Practice/FRQ/2016/RandomLetterChooser.java
+++ AP CompSci Practice/FRQ/2016/RandomLetterChooser.java
@@ -2,23 +2,7 @@ import java.util.ArrayList;
 
 public class RandomLetterChooser extends RandomStringChooser
 {
-    private ArrayList<String> Array;
-    private String received;
-    private int length;
     public RandomLetterChooser(String str)
     {
-        received = str;
-        length = str.length();
-        for(int i =0; i<length; i++)
-        {
-            Array.add(str.substring(i));
-        }
-        ArrayList<String> Arrayone = Array;
-        return Arrayone;
+    RandomLetterChooser.sChooser(getSingleLetters(str));
     }
-    public static String[] getSingleLetters(String str)
-    {
-        // put your code here
-        
-    }
-}
